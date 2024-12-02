c9cf20491e50f9bd46004deed60fb2a3c5eaf1ca
ecourse <smkang@codenuri.co.kr>
2024-12-02 16:06:22 +0900

    Add files via upload

---
+++ DAY1/6_Edit1.cpp
@@ -0,0 +1,41 @@
+﻿// 33 page ~ 
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+class Edit
+{
+	std::string data;
+public:
+	std::string get_data()
+	{
+		data.clear();
+
+		while (1)
+		{
+			char c = _getch(); // 한자 입력
+
+			if (c == 13) break;
+
+			if ( isdigit(c)  )
+			{
+				data.append(c);
+				std::cout << c;
+			}
+		}
+		std::cout << '\n';
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;
+
+	while (1)
+	{
+		std::cout << e.get_data() << std::endl;
+	}
+}
+
+
