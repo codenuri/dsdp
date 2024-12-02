6a04f3cc07012ef2cb85449a4ba744416edfc57d
ecourse <smkang@codenuri.co.kr>
2024-12-02 14:50:49 +0900

    Add files via upload

--- DAY1/5_도형편집기8.cpp
+++ DAY1/5_도형편집기8.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-
+class not_implemented {}; //예외를 위한 클래스
 class Shape
 {
 	int color;
@@ -18,19 +18,25 @@ public:
 
 	}
 
+	// 가상 함수      : 파생클래스가 override 하지 않으면 기본 구현 제공하겠다는 의도
+	// 순수 가상 함수 : 파생 클래스에게 반드시 override 하라고 지시하는 것
+
+	virtual void draw_imp() = 0;
 
 
-	virtual void draw_imp()
-	{
-		std::cout << "draw Shape\n";
-	}
+	// 아래 함수도
+	// 1. 순수 가상함수로 해도 좋습니다.
+	// 2. 예외 전달 구현도 나쁘지 않습니다.
+	// virtual Shape* clone() = 0; 
 
-	virtual Shape* clone()
-	{
-		Shape* s = new Shape;
-		s->color = color;
-		return s;
-	}
+	// 1. 파생 클래스가 만드시 만들 필요는 없다.
+	// 2. 파생클래스가 override 하지 않았는데 사용하지 않으면 ok
+	// 3. 파생클래스가 override 하지 않았는데 사용하면 예외 발생
+	// 4. 파생클래스가 override 고  사용하면 ok
+	// => 즉, 사용하려면 override 하라는 의미
+	virtual Shape* clone() { throw not_implemented(); }
+
+
 
 	virtual int get_area() { return -1; }
 };
