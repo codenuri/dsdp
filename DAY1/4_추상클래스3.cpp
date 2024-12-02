238644390f79442dc59accf0b238fb0309cbeca9
ecourse <smkang@codenuri.co.kr>
2024-12-02 15:28:45 +0900

    Add files via upload

--- DAY1/4_추상클래스3.cpp
+++ DAY1/4_추상클래스3.cpp
@@ -14,35 +14,21 @@ public:
 class People
 {
 public:
-	void use_camera(? c) { c->take(); }
+	void use_camera(ICamera* c) { c->take(); }
 };
 
 
-
-
-
-
-
-
-
-
-class Camera
+// 모든 카메라는 규칙을 지켜야 합니다.
+class Camera : public ICamera
 {
 public:
-	void take() { std::cout << "Take Picture" << std::endl; }
+	void take() override { std::cout << "Take Picture" << std::endl; }
 };
 
-class HDCamera
+class HDCamera : public ICamera
 {
 public:
-	void take() { std::cout << "Take HD Picture" << std::endl; }
-};
-
-class People
-{
-public:
-	void use_camera(Camera* p) { p->take(); }
-	void use_camera(HDCamera* p) { p->take(); }
+	void take() override { std::cout << "Take HD Picture" << std::endl; }
 };
 
 int main()
@@ -53,6 +39,6 @@ int main()
 	p.use_camera(&c);
 
 	HDCamera hc;
-	p.use_camera(&hc); // ??
+	p.use_camera(&hc); 
 }
 
