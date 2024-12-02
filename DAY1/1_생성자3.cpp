bdefb9b29d79e21ec14e0527a14c182727ae2ae9
ecourse <smkang@codenuri.co.kr>
2024-12-02 09:26:56 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -10,8 +10,16 @@ public:
 	// 클래스 설계시, 항상 생성자에서 멤버 데이타를 초기화하는 것이
 	// 좋습니다.
 	Person(const std::string& name, int age)
-		: name(name), age(age) { }
+		: name(name), age(age) {}
 };
+
+// 1. Person 에서 상속 받는 Student 만들어 보세요
+// 2. int id 멤버 데이타 추가하세요
+// 3. Student 생성자 만들어 보세요(id 초기화)
+// 4. main 에서 Student 객체 생성해 보세요
+
+
+
 
 int main()
 {
