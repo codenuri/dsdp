6d4070e09dfa97d5299c190af50bdad89fc83533
ecourse <smkang@codenuri.co.kr>
2024-12-02 14:26:57 +0900

    Add files via upload

---
+++ DAY1/3_가상함수2-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// 아래 와 같은 코드가 없으면 소멸자가 가상이 아니어도 됩니다.
+// => 가상함수를 느립니다.
+// Base* p = new Derived;
+// delete p;
+
+// 또다른 해결책
+
+class Base
+{
+
+protected:
+	~Base() {}	
+};
+
+class Derived : public Base
+{
+public:
+	Derived() { std::cout << "Derived() 자원할당" << std::endl; }
+	~Derived() { std::cout << "~Derived() 자원해지" << std::endl; }
+};
+int main()
+{
+	Base* p = new Derived;
+	delete p;	// 컴파일 에러, protected 소멸자
+
+	delete  static_cast<Derived*>(p); // ok
+
+}
+
+ 
