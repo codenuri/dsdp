ad1313f0404fced17f42f634a6a04062fdabb7ca
ecourse <smkang@codenuri.co.kr>
2024-12-02 12:41:36 +0900

    Add files via upload

--- DAY1/5_도형편집기2.cpp
+++ DAY1/5_도형편집기2.cpp
@@ -27,15 +27,20 @@ int main()
 		int cmd;
 		std::cin >> cmd;
 
-		if      (cmd == 1)  v.push_back(new Rect)
+		if      (cmd == 1) v.push_back(new Rect)
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
 			for (auto p : v)
-				p->draw();
+				p->draw();	// A. Error
 		}		
 	}
 }
 // 위 코드는 왜 ?? 어디서 ?? 에러 일까요 ??
+// => vector<Shape*> 이므로 
+// => 코드 A 에서 p 의 타입은 Shape* 이다.
+// => 그런데, Shape 에는 draw 가 없다!! 그래서 에러
+
 
 // 해결책은 뭘까요 ?
+// => 다음소스에서..., 
