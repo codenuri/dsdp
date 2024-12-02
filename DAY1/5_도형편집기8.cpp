49fa3b4d68155071c1be3fe30ad099d6bca51782
ecourse <smkang@codenuri.co.kr>
2024-12-02 15:01:57 +0900

    Add files via upload

--- DAY1/5_도형편집기8.cpp
+++ DAY1/5_도형편집기8.cpp
@@ -32,12 +32,16 @@ public:
 	// 1. 파생 클래스가 만드시 만들 필요는 없다.
 	// 2. 파생클래스가 override 하지 않았는데 사용하지 않으면 ok
 	// 3. 파생클래스가 override 하지 않았는데 사용하면 예외 발생
-	// 4. 파생클래스가 override 고  사용하면 ok
+	// 4. 파생클래스가 override 하고  사용하면 ok
 	// => 즉, 사용하려면 override 하라는 의미
 	virtual Shape* clone() { throw not_implemented(); }
 
 
-
+	// 아래 함수도
+	// 1. =0 으로 해도 되고
+	// 2. 기본 구현으로 예외 던지는 기법도 가능하고
+	// 3. 실패를 나타내는 값을 약속(C기법)한 것도 가능.. 
+	// => "get_area()의 결과가 -1이면 면적 구할수 없음"이라고 약속하자는것
 	virtual int get_area() { return -1; }
 };
 
@@ -48,24 +52,18 @@ public:
 class Rect : public Shape
 {
 public:
-	void draw_imp() { std::cout << "draw Rect\n"; }
+	void draw_imp() override { std::cout << "draw Rect\n"; }
 
-	virtual Shape* clone()
-	{
-		return new Rect(*this);
-	}
+	Shape* clone() override  { return new Rect(*this);	}
 };
 
 
 class Circle : public Shape
 {
 public:
-	void draw_imp() { std::cout << "draw Circle\n"; }
+	void draw_imp() override  { std::cout << "draw Circle\n"; }
 
-	virtual Shape* clone()
-	{
-		return new Circle(*this);
-	}
+	Shape* clone() override  {	return new Circle(*this);}
 };
 
 int main()
@@ -77,24 +75,28 @@ int main()
 		int cmd;
 		std::cin >> cmd;
 
+		// 생각해 볼 문제 #1. 객체의 생성과정을 OCP 를 만족하게 할수 없을까 ?
+		// => 새로운 도형이 추가되어도 아래 코드가 수정되지 않게 ??
+		// => "factory" 패턴을 사용하면 됩니다. 목요일날 배우게 됩니다.
+
+		// #2. undo/redo 기능을 넣으려면 어떻게 해야 할까 ?
+		// => "command" 패턴을 배우면 됩니다. 목요일날 배우게 됩니다.
 		if (cmd == 1) v.push_back(new Rect);
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
 			for (auto p : v)
 			{
-				p->draw();
+				p->draw();	// 다형성. OCP 만족
+							// 새로운 도형 추가시 이 한줄은 수정될 필요 없다.
 			}
 		}
-
 		else if (cmd == 8)
 		{
 			std::cout << "몇번째 만든 도형을 복제 할까요 >> ";
-
 			int k;
 			std::cin >> k;
-
-			v.push_back(v[k]->clone());
+			v.push_back(v[k]->clone());// 다형성, OCP 만족, 좋은 코드
 
 
 		}
