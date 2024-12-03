bb7c65d5afa3c59c7e54a15e344028fad306c99d
ecourse <smkang@codenuri.co.kr>
2024-12-03 09:39:06 +0900

    Add files via upload

--- DAY2/1_단위전략4.cpp
+++ DAY2/1_단위전략4.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <memory> // std::allocator 가 여기에 있습니다.
 
 // 방법 #3. 단위전략 디자인(Policy Base Design)
 
@@ -8,9 +9,11 @@
 // strategy : 인터페이스를 만들어서 교체 가능하게 하자.
 //			  Edit/IValidator 예제
 //			  대부분의 객체지향 언어가 사용
+//			  => 디자인 패턴 23개중 한개
 
 // policy base design : template 인자로 교체 가능하게 하자.
 //			  C++ 진영에서만 사용되는 디자인 기술
+//			=> C++ IDioms 에 있는 용어. 디자인 패턴 아님.
 
 
 
@@ -18,7 +21,7 @@
 
 // IDioms : 언어의 특징을 고려한 디자인 기술
 //          C++ IDioms, Java Idioms, C# IDioms 등... 
-
+// 구글에서 "C++ IDioms" 검색, 1번째 링크
 
 
 
