e0b3b1e5b46b86ec2725c61fec2f0bc9023ea70f
ecourse <smkang@codenuri.co.kr>
2024-12-03 13:05:58 +0900

    Add files via upload

--- DAY2/2_메뉴6.cpp
+++ DAY2/2_메뉴6.cpp
@@ -83,26 +83,33 @@ public:
 int main()
 {
 	PopupMenu* root = new PopupMenu("ROOT");
-
 	PopupMenu* pm1 = new PopupMenu("색상 변경");
 	PopupMenu* pm2 = new PopupMenu("해상도 변경");
 
 	root->add(pm1);
-	root->add(pm2);
+//	root->add(pm2); // 메뉴 구조의 변경이
+	pm1->add(pm2);	// 아주 간단합니다.
 
 	pm1->add(new MenuItem("RED",  11));
 	pm1->add(new MenuItem("GREEN", 12));
 	pm1->add(new MenuItem("BLUE",  13));
+	pm1->add(new MenuItem("WHITE", 14));
 
 	pm2->add(new MenuItem("HD", 21));
 	pm2->add(new MenuItem("FHD", 22));
 	pm2->add(new MenuItem("UHD", 23));
 
 	// 이제 메뉴를 시작 하려면 ?
-	// ???
-
+	root->command();
 }
 
+// 객체지향 프로그램에서 "프로그램이란 ??"
+
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는 과정이다.(서로의 멤버 함수 호출 한다는 의미)
+
+// 4. 프로그램의 기본 단위는 "함수" 가 아니라 "클래스" 이다.
 
 
 
