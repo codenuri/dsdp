c2df54017b3b81aee5412031c4707c8784a288b6
ecourse <smkang@codenuri.co.kr>
2024-12-03 09:33:54 +0900

    Add files via upload

--- DAY2/1_단위전략4.cpp
+++ DAY2/1_단위전략4.cpp
@@ -2,6 +2,27 @@
 
 // 방법 #3. 단위전략 디자인(Policy Base Design)
 
+// 변하는 것(메모리 할당 방식) 을 다른 클래스로 분리!!
+
+// 어떻게 교체 가능하게 할것인가 ?
+// strategy : 인터페이스를 만들어서 교체 가능하게 하자.
+//			  Edit/IValidator 예제
+//			  대부분의 객체지향 언어가 사용
+
+// policy base design : template 인자로 교체 가능하게 하자.
+//			  C++ 진영에서만 사용되는 디자인 기술
+
+
+
+// 디자인 패턴 : 대부분에 객체지향 언어에 적용 가능한 기술, 23개
+
+// IDioms : 언어의 특징을 고려한 디자인 기술
+//          C++ IDioms, Java Idioms, C# IDioms 등... 
+
+
+
+
+
 // std::allocator<T> : C++ 표준에 이미 있는 메모리 할당기
 //					   내부적으로 new, delete 사용
 template<typename T, typename Alloc = std::allocator<T> >
