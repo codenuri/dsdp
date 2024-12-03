848993fa03b9ac6ca50e48865a0147d48d837b8f
ecourse <smkang@codenuri.co.kr>
2024-12-03 17:06:41 +0900

    Add files via upload

--- DAY2/5_Adapter5.cpp
+++ DAY2/5_Adapter5.cpp
@@ -14,8 +14,14 @@ public:
 	inline void pop() { c.pop_back(); }
 	inline T& top() { return c.back(); }
 };
+#include <stack> // 이안에 코드가 위와 거의 동일 합니다.
+				 // stack adapter 라고 부릅니다.
+				 // cppreference.com 에서 Container Library 선택
 int main()
 {
+	std::stack<int, std::list<int>> s3;
+	
+
 	stack<int, std::list<int>> s1;  
 	stack<int, std::vector<int>> s2; // vector 를 stack 처럼 사용하겠다는
 									// 의도. 
