0026981d1727c7fbb936891abeea93c89b2e06f0
ecourse <smkang@codenuri.co.kr>
2024-12-03 16:37:45 +0900

    Add files via upload

---
+++ DAY2/5_Adapter2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// 아래 클래스가 이미 있었다가 가정해 봅시다.
+// => 문자열을 보관했다가 화면에 이쁘게 출력하는 기능
+class TextView
+{
+	// font 종류, 크기, 색상 등... 다양한 정보 관리 
+	std::string data;
+public:
+	TextView(const std::string& s) : data(s) {}
+
+	void show() { std::cout << data << std::endl; }
+};
+
+
+
+
+//=================================================
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle" << std::endl; }
+};
+
+
+// 클래스 어답터 : 클래스 인터페이스 변경
+// 객체   어답터 : 객체   인터페이스 변경
+
+class Text : public TextView,  public Shape     
+{
+public:
+	Text(const std::string& s) : TextView(s) {}
+
+	void draw() override
+	{
+		TextView::show();
+	}
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	v.push_back(new Text("Hello"));
+
+	v[0]->draw();
+}
+
+
+
+
+
+
+
