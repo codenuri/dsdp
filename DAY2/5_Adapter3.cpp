5a8ca3552d697f83338c5fa6758ccb8783a59a56
ecourse <smkang@codenuri.co.kr>
2024-12-03 16:56:06 +0900

    Add files via upload

--- DAY2/5_Adapter3.cpp
+++ DAY2/5_Adapter3.cpp
@@ -12,14 +12,17 @@ template<typename T>
 class stack : public std::list<T>
 {
 public:
-	void push(const T& e)	{ std::list<T>::push_back(e); }
-	void pop()				{ std::list<T>::pop_back(); }
-	T& top()                { return std::list<T>::back(); }
+	inline void push(const T& e)	{ std::list<T>::push_back(e); }
+	inline void pop()				{ std::list<T>::pop_back(); }
+	inline T& top()                { return std::list<T>::back(); }
 };
-
 
 int main()
 {
 	stack<int> s;
 	s.push(10);
+
+	s.push_front(20); // 사용자가 실수했다.
+				      // 스택은 한쪽으로만 입출력되어야 한다.
+					  // 해결책은 다음소스에서
 }
