6125e1f0c1d324bdfed592b9132828a1776331f4
ecourse <smkang@codenuri.co.kr>
2024-12-03 09:27:45 +0900

    Add files via upload

---
+++ DAY2/1_단위전략4.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+
+// 방법 #3. 단위전략 디자인(Policy Base Design)
+
+
+template<typename T, typename Alloc>
+class vector
+{
+	T* buff = nullptr;
+	int size;
+
+//	MallocAllocator<T> ax; // 이렇게 하면 강한 결합이므로 교체 안됨
+
+	Alloc ax; // 템플릿 인자로 전달된 타입 사용
+
+public:
+	vector(int sz ) : size(sz)
+	{
+		buff = ax.allocate(sz);
+	}
+	~vector()
+	{
+		ax.deallocate(buff, size);
+	}
+};
+
+template<typename T> class MallocAllocator 
+{
+public:
+	inline T* allocate(int sz)
+	{
+		return static_cast<T*>(malloc(sizeof(T) * sz));
+	}
+	inline void deallocate(T* p, int sz)
+	{
+		free(p);
+	}
+};
+
+int main()
+{
+	vector<int, MallocAllocator<int>> v(4); 
+}
+
+
