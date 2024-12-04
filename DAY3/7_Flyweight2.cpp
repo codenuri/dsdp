efca87cd2211c6181b72421f4f38e3655b03026b
ecourse <smkang@codenuri.co.kr>
2024-12-04 16:47:11 +0900

    Add files via upload

--- DAY3/7_Flyweight2.cpp
+++ DAY3/7_Flyweight2.cpp
@@ -31,18 +31,20 @@ public:
 		else
 			img = it->second; // map 의 반복자는 pair 인데
 							  // second 가 value(Image*) 입니다.
-
 		return img;
 	}
 
-	static std::map<std::string, Image*> image_map;
+	// C++17 inline static 문법
+	// => static 멤버 데이타 만들때 외부 선언 없어도 됩니다.
+	inline static std::map<std::string, Image*> image_map;
 };
 
-std::map<std::string, Image*> Image::image_map;
 
 
 int main()
 {
+	// IOS 용 라이브러리인 Cocoa Touch 라이브러리가 아래 처럼 
+	// Image 객체를 생성합니다.
 	Image* img1 = Image::create("www.naver.com/a.png");
 	img1->draw();
 
