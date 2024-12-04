8659bd80168e8b16235a3b319e1496ff9ca5383c
ecourse <smkang@codenuri.co.kr>
2024-12-04 14:10:36 +0900

    Add files via upload

--- DAY3/5_command4.cpp
+++ DAY3/5_command4.cpp
@@ -31,43 +31,47 @@ struct ICommand
 	virtual ~ICommand() {}
 };
 
-class AddRectCommand : public ICommand
+// 도형을 추가하는 명령은 유사한점이 많이 있습니다.
+// 기반 클래스로 제공합니다.
+class AddCommand : public ICommand
 {
 	std::vector<Shape*>& v;
 public:
-	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
+	AddCommand(std::vector<Shape*>& v) : v(v) {}
 
-	void execute() override { v.push_back(new Rect); }
-	bool can_undo() override { return true; }
-
-	void undo() override
-	{
-		Shape* s = v.back(); 
-		v.pop_back();
-
-		delete s;
-	}
-};
-
-
-class AddCircleCommand : public ICommand
-{
-	std::vector<Shape*>& v;
-public:
-	AddCircleCommand(std::vector<Shape*>& v) : v(v) {}
-
-	void execute() override { v.push_back(new Circle); }
+	void execute() override { v.push_back( create_shape() ); }
 	bool can_undo() override { return true; }
 
 	void undo() override
 	{
 		Shape* s = v.back();
-
 		v.pop_back();
-
 		delete s;
 	}
+
+	virtual Shape* create_shape() = 0;
 };
+
+
+class AddRectCommand : public AddCommand
+{
+public:
+	AddRectCommand(std::vector<Shape*>& v) : AddCommand(v) {}
+
+	Shape* create_shape() override { return new Rect; }
+};
+
+class AddCircleCommand : public AddCommand
+{
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : AddCommand(v) {}
+
+	Shape* create_shape() override { return new Circle; }
+};
+
+
+
+
 
 class DrawCommand : public ICommand
 {
