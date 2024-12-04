5978df2364af15d0def9165132c861543e14aaf5
ecourse <smkang@codenuri.co.kr>
2024-12-04 16:52:10 +0900

    Add files via upload

--- DAY3/7_Flyweight3.cpp
+++ DAY3/7_Flyweight3.cpp
@@ -15,33 +15,46 @@ public:
 
 	void draw() { std::cout << "Draw " << image_url << std::endl; }
 
-	static Image* create(const std::string& url)
+	friend class ImageFactory;	// 이 클래스에서는
+								// Image 의 private 접근 가능
+};
+
+
+
+// Image : 그림을 다운로드후 그리는 기능만 가지게 하고
+// ImageFactory : Image  객체의 생성과 공유를 담당
+
+class ImageFactory
+{
+	std::map<std::string, Image*> image_map;
+public:
+	Image* create(const std::string& url)
 	{
 		Image* img = nullptr;
 
-		auto it = image_map.find(url); 
+		auto it = image_map.find(url);
 
-		if (it == image_map.end()) 
+		if (it == image_map.end())
 		{
 			img = new Image(url);
 			image_map[url] = img;
 		}
 		else
-			img = it->second; 
+			img = it->second;
 		return img;
 	}
 
-	inline static std::map<std::string, Image*> image_map;
 };
-
 
 
 int main()
 {
-	Image* img1 = Image::create("www.naver.com/a.png");
+	ImageFactory factory;
+
+	Image* img1 = factory.create("www.naver.com/a.png");
 	img1->draw();
 
-	Image* img2 = Image::create("www.naver.com/a.png");
+	Image* img2 = factory.create("www.naver.com/a.png");
 	img2->draw();
 
 }
