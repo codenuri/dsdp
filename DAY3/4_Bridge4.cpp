813b66680d76fd4e8d0fae53544fb4cd09d28790
ecourse <smkang@codenuri.co.kr>
2024-12-04 13:01:52 +0900

    Add files via upload

--- DAY3/4_Bridge4.cpp
+++ DAY3/4_Bridge4.cpp
@@ -20,7 +20,9 @@ void PointImpl::print() {}
 // 구현자체는 없지만
 // 사용자는 이 클래스 사용합니다.
 // Point.h
-
+//#include "PointImpl.h"
+class PointImpl; // 클래스 전방선언만 있어도
+				 // 포인터 변수는 사용가능
 class Point
 {
 	PointImpl* impl;
@@ -31,6 +33,8 @@ public:
 
 // Point.cpp
 #include "Point.h"
+#include "PointImpl.h"
+
 Point::Point() { impl = new PointImpl;  }
 void Point::print() { impl->print(); }
 
@@ -45,4 +49,17 @@ int main()
 	p.print();
 }
 
+// 위 기술은 결국 "Bridge" 패턴인데
 
+// C++에서는 "PIMPL" 이라고도 부릅니다.
+
+// "Pointer to IMPLementation"
+
+// 장점 1. 컴파일러 방화벽,컴파일 속도 증가
+//		  PointImpl.h 가 변해도
+//		  최종 사용자 코드는 재 컴파일 필요 없다.
+//        (중간층만 재컴파일 하면 된다.)
+
+// 장점 2. 완벽한 정보 은닉
+//		  구현의 헤더를 노출할 필요 없다.
+//		  간접층의 헤더만 노출하면된다.
