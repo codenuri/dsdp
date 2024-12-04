97017e35d944f4a06aff357ac4293ce50e23359a
ecourse <smkang@codenuri.co.kr>
2024-12-04 15:01:07 +0900

    Add files via upload

---
+++ DAY3/6_싱글톤2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+// static 지역변수 vs static 멤버 데이타 
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+
+//	Cursor instance;		// 이코드는 에러 지만
+	static Cursor instance; // 이 코드는 에러 아닙니다.
+
+public:
+	static Cursor& get_instance()
+	{
+//		static Cursor instance;
+
+		return instance;
+	}
+
+};
+Cursor Cursor::instance;
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+
+}
+
+
+
+
+
+
+
