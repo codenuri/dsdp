1572337ea0ad8b8c5e877efc8c650df59921b0d8
ecourse <smkang@codenuri.co.kr>
2024-12-04 09:50:17 +0900

    Add files via upload

---
+++ DAY3/CalcProxy.cpp
@@ -0,0 +1,12 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+class Calc : public ICalc
+{
+	int server;
+public:
+	Calc() { server = ec_find_server("Calc"); }
+
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); };
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); };
+};
