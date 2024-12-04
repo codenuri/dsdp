81307dc104adb2dc247e228a8fcae76e042bca72
ecourse <smkang@codenuri.co.kr>
2024-12-04 16:12:08 +0900

    Add files via upload

--- DAY3/6_싱글톤7.cpp
+++ DAY3/6_싱글톤7.cpp
@@ -16,12 +16,24 @@ public:
 	// => 오직 한개의 객체만 생성되고, Cursor 의 생성자 호출이 
 	//    동기화 됨을 보장할수 있을까요 ?
 
+	// C++11 부터 static 지역변수의 생성자 호출은
+	// "언어" 자체에서 동기화 됨을 보장합니다.
+	// 동기화 방식은 "spinlock(busy wait)" 입니다
+
 	static Cursor& get_instance()
 	{
+
 		static Cursor instance;
+		
 		return instance;
+
 	}
 };
+
+
+
+
+
 
 int main()
 {
