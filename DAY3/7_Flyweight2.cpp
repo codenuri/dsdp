9a3ce609db860e29c2c01c21de8c6bbc457c438b
ecourse <smkang@codenuri.co.kr>
2024-12-04 16:40:25 +0900

    Add files via upload

--- DAY3/7_Flyweight2.cpp
+++ DAY3/7_Flyweight2.cpp
@@ -19,10 +19,26 @@ public:
 	// 자신의 객체를 만드는 static 멤버 함수
 	static Image* create(const std::string& url)
 	{
-		Image* img = new Image(url);
+		Image* img = nullptr;
+
+		auto it = image_map.find(url); // map 에서 먼저 검색
+
+		if (it == image_map.end()) // 없을때
+		{
+			img = new Image(url);
+		}
+		else
+			img = it->second; // map 의 반복자는 pair 인데
+							  // second 가 value(Image*) 입니다.
+
 		return img;
 	}
+
+	static std::map<std::string, Image*> image_map;
 };
+
+std::map<std::string, Image*> Image::image_map;
+
 
 int main()
 {
