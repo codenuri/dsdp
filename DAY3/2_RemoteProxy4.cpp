1e5a8e35c5ff5179aa7537ebae4b885f0895e713
ecourse <smkang@codenuri.co.kr>
2024-12-04 10:13:48 +0900

    Add files via upload

--- DAY3/2_RemoteProxy4.cpp
+++ DAY3/2_RemoteProxy4.cpp
@@ -7,24 +7,6 @@
 
 #include "ICalc.h"
 
-ICalc* load_proxy()
-{
-	// #1. DLL 을 Load 합니다
-	void* addr = ec_load_module("CalcProxy.dll");
-					// linux : dlopen()    windows : LoadLibrary()
-
-	// #2. DLL 에서 약속된 함수를 찾습니다.
-	typedef ICalc* (*F)();
-
-	F f = (F)ec_get_function_address(addr, "create");
-					// linux : dlsym()		windows : GetProcAddress()
-
-	// #3. 약속된 함수로 Proxy 객체 생성후 반환
-
-	return f();
-}
-
-
 
 int main()
 {
