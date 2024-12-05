0436ae822bb5df43f1a0da4a91121c87788b2782
ecourse <smkang@codenuri.co.kr>
2024-12-05 14:38:37 +0900

    Add files via upload

--- DAY4/6_iterator8.cpp
+++ DAY4/6_iterator8.cpp
@@ -10,16 +10,17 @@ public:
 	take_view(T& r, int c) : rng(r), cnt(c) {}
 
 	auto begin() { return rng.begin(); }
-	auto end() { return rng.begin() + cnt; }
+	auto end() { return rng.begin() + cnt; } // ÇÙ½É 
 };
 
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
-	take_view tv(v, 3);
+//	take_view tv(v, 3);
 
 //	for (auto e : v)
-	for (auto e : tv)
+//	for (auto e : tv)
+	for (auto e : take_view(v, 3) )
 	{
 		std::cout << e << ", ";
 	}
