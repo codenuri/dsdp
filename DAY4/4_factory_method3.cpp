c6fe45f38a1482978f3a9f8764377fd34ad9d6bd
ecourse <smkang@codenuri.co.kr>
2024-12-05 12:45:05 +0900

    Add files via upload

--- DAY4/4_factory_method3.cpp
+++ DAY4/4_factory_method3.cpp
@@ -33,13 +33,32 @@ struct OSXEdit : public IEdit
 };
 //--------------------------------------
 
+// 각 스타일의 컨트롤의 이름만 관리하는 타입(구조체) 설계
+struct WinStyle
+{
+	// 타입(클래스)의 이름만 약속된 별명으로 관리하는 구조체
+	using Button = WinButton;
+	using Edit = WinEdit;
+
+	// 그외 모든 Windows 스타일 컨트롤의 이름만 관리 합니다.
+};
+
+struct OSXStyle
+{
+	using Button = OSXButton;
+	using Edit = OSXEdit;		// typedef OSXEdit Edit;
+};
+
+
+
+template<typename T>
 class Dialog
 {
 public:
 	void init()
 	{
-		IButton* btn = CreateButton();
-		IEdit* edit = CreateEdit();
+		IButton* btn = new typename T::Button;
+		IEdit* edit = new typename T::Edit;
 
 		// btn->Move(); edit->Move();
 
@@ -52,7 +71,8 @@ public:
 
 int main(int argc, char** argv)
 {
-
+	Dialog<WinStyle> dlg1;
+	Dialog<OSXStyle> dlg2;
 }
 
 
