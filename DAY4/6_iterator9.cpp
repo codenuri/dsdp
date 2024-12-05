014114e45c34b8719068183c02eb11d5234bc514
ecourse <smkang@codenuri.co.kr>
2024-12-05 14:41:30 +0900

    Add files via upload

---
+++ DAY4/6_iterator9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+// 앞에서 만든 take_view 같은 개념(컨테이너를 바라 보는 제한된 시각, 복사본생성안함)
+// 이 C++, Java, C# 등에 모두 있습니다.
+// C++ : Range  (C++20 부터) - C++언어 20으로 설정하세요
+// C#  : LINQ
+// Java : Stream 
+
+#include <ranges>
+
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	using RG = std::ranges;
+
+//	for (auto e : RG::take_view(v, 3))
+	for (auto e : RG::reverse_view(v))
+	{
+		std::cout << e << ", ";
+	}
+
+}
