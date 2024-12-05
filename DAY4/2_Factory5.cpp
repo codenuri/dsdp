31b07c378fabedda848dabdfccc1d66aa1dc32c7
ecourse <smkang@codenuri.co.kr>
2024-12-05 10:27:40 +0900

    Add files via upload

--- DAY4/2_Factory5.cpp
+++ DAY4/2_Factory5.cpp
@@ -73,8 +73,18 @@ int main()
 
 	// 공장에 제품을 등록합니다.
 	// => 도형번호와 생성함수 등록
-	factory.register_shape(1, &Rect::create);
-	factory.register_shape(2, &Circle::create);
+	// => 결국, 공장에 "클래스이름"을 등록하는 효과 입니다.
+//	factory.register_shape(1, &Rect::create);
+//	factory.register_shape(2, &Circle::create);
+
+	// 공장에 "클래스"가 아닌 "자주 사용되는 제품"의 견본을 등록해 봅시다.
+	Rect* redRect = new Rect;
+	Rect* blueRect = new Rect;
+	Circle* redCircle = new Circle;
+
+	factory.register_shape(1, redRect);
+	factory.register_shape(2, blueRect);
+	factory.register_shape(3, redCircle);
 
 
 	while (1)
