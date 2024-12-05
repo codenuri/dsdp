09616b9896f25bd7b566c21a48455f79ea95f57a
ecourse <smkang@codenuri.co.kr>
2024-12-05 15:36:21 +0900

    Add files via upload

--- DAY4/7_visitor2.cpp
+++ DAY4/7_visitor2.cpp
@@ -37,15 +37,20 @@ template<typename T>
 class MyList : public std::list<T>,	public IAcceptor<T>
 {
 public:
+	// 생성자 상속 문법
+	// using 클래스이름::생성자 이름;
+	using std::list<T>::list; // list 의 모든 생성자를 
+							  // MyList 도 그대로 사용할수 있게 해달라.
+
 	void accept(IVisitor<T>* visitor)
 	{
 		for (auto& e : *this)  // *this 를 잘생각해보세요
-		{
+		{						// MyList 는 결국 std::list 로 상속 받으므로
+								// *this 는 list 라고 생각하면 됩니다.
 			visitor->visit(e);
 		}
 	}
 };
-
 
 int main()
 {
