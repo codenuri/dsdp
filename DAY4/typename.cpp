9f55a69752f857ff47a7003b7d4517c0ba3423da
ecourse <smkang@codenuri.co.kr>
2024-12-05 12:50:48 +0900

    Add files via upload

--- DAY4/typename.cpp
+++ DAY4/typename.cpp
@@ -4,13 +4,26 @@ class Sample
 	inline static int value = 10;
 	using DWORD = int;
 };
-
 int p = 0;
 
 template<typename T>
 void foo()
 {
 	// 아래 코드에서 * 의 의미를 생각해 보세요
-	Sample::value* p;
-	Sample::DWORD* p;
+//	Sample::value* p;		// 곱하기 의미
+//	Sample::DWORD* p;		// 포인터 변수의 선언
+
+	// "클래스이름::이름" 에서 "이름" 은
+	// 1. 값 일수 있다 : static 멤버 데이타, enum 상수등..
+	// 2. 타입일수 있다 : 별명(typedef, using)
+	// => 이름의 의미에 따라 * 등의 연산자의 의미가 달라진다.
+
+	// 아래 코드를 컴파일러가 어떻게 해석해야 할까요 ?
+	T::value* p;
+	T::DWORD* p;
+}
+
+int main()
+{
+
 }
