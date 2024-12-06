15afba6fa69626dfcc25f95b2c9f695be8de7475
ecourse <smkang@codenuri.co.kr>
2024-12-06 12:31:37 +0900

    Add files via upload

--- DAY5/5_State2.cpp
+++ DAY5/5_State2.cpp
@@ -27,7 +27,12 @@ int main()
 	p->run();
 	p->attack();
 
-	p = new SuperCharacter;
+	p = new SuperCharacter; // 기존 캐릭터 객체의 동작이 변경된것이 아니라
+							// 동작이 다른 새로운 캐릭터 객체 생성
+							// => 동작만 바꾼것이 아니라, 상태도 변경된것
+
+							// => 원하는 것은 상태는 유지 하는데, 
+							//    동작(멤버함수)만 바꾸고 싶은것 !!
 	p->run();
 	p->attack();
 }
