7008d7a8d71573e25d702e0f059154903717b3f8
ecourse <smkang@codenuri.co.kr>
2024-12-06 09:56:00 +0900

    Add files via upload

--- DAY5/4_GUI2.cpp
+++ DAY5/4_GUI2.cpp
@@ -2,15 +2,7 @@
 #include "cppmaster.h" 
 
 
-int foo(int hwnd, int msg, int a, int b)
-{
-	switch (msg)
-	{
-	case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
-	case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
-	}
-	return 0;
-}
+
 
 class Window
 {
@@ -18,8 +10,26 @@ class Window
 public:
 	void create(const std::string& title)
 	{
+		handle = ec_make_window(msgproc, title);
+
+		ec_set_window_rect(handle, 0, 0, 300, 300);
+	}
+
+	// 핵심 : 아래 함수가 static 이어야 하는 이유를 반드시 알아야 합니다.
+	// => C언어에서 다른 함수의 인자로 전달되는 함수(Call back 함수)
+	//    를 C++ 언어로 만들때는 "반드시 static 멤버 함수" 이어야 합니다.
+
+	static int msgproc(int hwnd, int msg, int a, int b)
+	{
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
+		case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
+		}
+		return 0;
 	}
 };
+
 
 int main()
 {
