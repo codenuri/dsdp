1fd30354bbceac35692543dbe63ad258ba0b7b46
ecourse <smkang@codenuri.co.kr>
2024-12-06 09:00:35 +0900

    Add files via upload

---
+++ DAY5/2_chain_of_responsibility1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+// Chain Of Responsibility
+// => 책임의 고리(전가) 정도로 번역..
+// => 많이 사용되는데, 잘알려지지 않은 패턴
+//    (많은 라이브러리가 다른 이름 사용해서)
+
+// => 발생된 문제(이벤트)를 고리에 따라 옆으로 전달
+// => 해결하면 중단될수도 있고, 해결되도 계속 옆으로 보낼수 있다.
+
+
+class Handler
+{
+	Handler* next = nullptr;
+public:
+	Handler* set_next(Handler* h)
+	{
+		next = h;
+		return next;
+	}
+	// 아래 함수가 핵심
+	void handle(int problem)
+	{
+		// #1. 자신이 먼저 해결을 시도
+		if (resolve(problem) == true)
+			return;
+
+		// #2. 처리되지 않은 경우 옆팀이 있으면 전달
+		
+		if (next != nullptr )
+			next->handle(problem);
+	}
+
+	virtual bool resolve(int problem) = 0;
+};
+// 위 클래스가 "Chain Of Responsibility" 의 기본 로직을 제공합니다.
+// 이제 문제를 해결할 각각의 파생 클래스를 만들면 됩니다.
+
+class Team1 : public Handler
+{
+public:
+	bool resolve(int problem)
+	{
+		std::cout << "Team1 Start\n";
+		if (problem == 7)
+		{
+			std::cout << "resolved by Team1\n";
+			return true;
+		}
+		return false;
+	}
+};
+
+
+
+int main()
+{
+}
