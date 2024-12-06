3e8097b977c71e1225d267037f731e1aadb4e676
ecourse <smkang@codenuri.co.kr>
2024-12-06 10:07:53 +0900

    Add files via upload

--- DAY5/4_GUI4.cpp
+++ DAY5/4_GUI4.cpp
@@ -1,5 +1,10 @@
 #define USING_GUI
 #include "cppmaster.h" 
+#include <map>
+
+class Window;
+
+std::map<int, Window*> this_map; // this를 보관하기 위한 맵
 
 
 class Window
@@ -11,19 +16,29 @@ public:
 		handle = ec_make_window(msgproc, title);
 
 		ec_set_window_rect(handle, 0, 0, 300, 300);
+
+		// 핵심. static 멤버함수인 msgproc 에서 this를 사용하기 위해
+		//      자료구조에 this를 보관합니다.
+		this_map[handle] = this;
 	}
 
 
 	static int msgproc(int hwnd, int msg, int a, int b)
 	{
+		Window* self = this_map[hwnd];
+
+		// 이제 self 가 객체의 주소인 this 의 의미 입니다.
+		// "self->" 로 접근하면
+		// static 멤버 함수인 이 안에서, 모든 멤버 접근 가능합니다.
+
 		switch (msg)
 		{
 		case WM_LBUTTONDOWN:
-			lbutton_down();		
+			self->lbutton_down();		
 			break;
 
 		case WM_KEYDOWN:     
-			key_down(); 
+			self->key_down();
 			break;
 		}
 		return 0;
