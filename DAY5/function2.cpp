5983d91313057c166b48f46d4f7b35e934accdec
ecourse <smkang@codenuri.co.kr>
2024-12-06 14:05:25 +0900

    Add files via upload

--- DAY5/function2.cpp
+++ DAY5/function2.cpp
@@ -14,8 +14,17 @@ void f4(int a, int b, int c, int d)
 int main()
 {
 	// C의 함수 포인터는 유연성이 부족합니다.
+	/*
 	void(*f)(int);
 
 	f = &f1; // ok.. signature가 동일
 	f = &f2; // error. 즉, 인자의 갯수가 다르면 담을수 없습니다.
+	*/
+
+	// C++ 에서는 함수 포인터의 일반화된 형태인 std::function<> 제공
+
+	std::function<void(int)> f; // void(*f)(int) 와 유사
+
+	f = &f1; // ok
+	f(10);   // f1(10).. 즉, 함수포인터와 동일하게 사용
 }
