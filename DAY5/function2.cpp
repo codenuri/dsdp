0a28d20057448e6c6636c1818d6d3b79714c8031
ecourse <smkang@codenuri.co.kr>
2024-12-06 14:03:09 +0900

    Add files via upload

---
+++ DAY5/function2.cpp
@@ -0,0 +1,21 @@
+// function1.cpp
+#include <iostream>
+#include <functional>
+using namespace std::placeholders; 
+
+void f1(int n1) {}
+void f2(int n1, int n2) {}
+
+void f4(int a, int b, int c, int d)
+{
+	printf("%d, %d, %d, %d\n", a, b, c, d);
+}
+
+int main()
+{
+	// C의 함수 포인터는 유연성이 부족합니다.
+	void(*f)(int);
+
+	f = &f1; // ok.. signature가 동일
+	f = &f2; // error. 즉, 인자의 갯수가 다르면 담을수 없습니다.
+}
