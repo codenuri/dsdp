4d55d665faa3a4e4a14eb0d90ae4a25a716d4f82
ecourse <smkang@codenuri.co.kr>
2024-12-06 10:02:39 +0900

    Add files via upload

---
+++ DAY5/4_GUI4.cpp
@@ -0,0 +1,44 @@
+#define USING_GUI
+#include "cppmaster.h" 
+
+
+class Window
+{
+	int handle;
+public:
+	void create(const std::string& title)
+	{
+		handle = ec_make_window(msgproc, title);
+
+		ec_set_window_rect(handle, 0, 0, 300, 300);
+	}
+
+
+	static int msgproc(int hwnd, int msg, int a, int b)
+	{
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN:
+			lbutton_down();		
+			break;
+
+		case WM_KEYDOWN:     
+			key_down(); 
+			break;
+		}
+		return 0;
+	}
+
+	virtual void lbutton_down() {}
+	virtual void key_down() {}
+};
+
+
+int main()
+{
+	Window w;
+	w.create("hello");
+
+	ec_process_message();
+}
+
